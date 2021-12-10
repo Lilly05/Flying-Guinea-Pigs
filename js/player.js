c2c4b1e9eb1a3cc00ec91c8a9712acd27164d698
Lilly05 <lilly.koller@icloud.com>
2021-12-10 13:25:49 +0100

    more functions

---
+++ js/player.js
@@ -0,0 +1,4 @@
+function Player(){
+    image(pig, 550, windowHeight / 2);
+    player.resize(0, 125);
+}
--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,12 @@
 let background, player, pipe_bottom, pipe_top;
 let game_size = [572, 1014];
 let game_running = false;
-let pipe = [200, 100];
-let pipe_gab = 100;
-let pipe_speed = 10;
+let pipe = [800, 280];
+let pipe_gab = 200;
+let pipe_speed = 5;
+let jump = 0;
+let playercoordinates = [100, 350];
+
 
 function preload() {
   // Lädt die Bilder
@@ -14,27 +17,41 @@ function preload() {
 }
 
 function setup() {
-  game_size = [windowWidth, windowHeight];
+  game_size = [450, windowHeight];
   createCanvas(game_size[0], game_size[1]);
   background.resize(0, game_size[1]);
   player.resize(0, 125);
-  frameRate(25);
 }
 
 function play() {
-  image(pipe_top, pipe[0], pipe[1] - pipe_top.height);
-  image(pipe_bottom, pipe[0], pipe[1] + pipe_gab);
+  pipe[0] -= pipe_speed;
+  if(pipe[0] < -pipe_top.width){
+    pipe[0] = game_size[0];
+    pipe[1] = 50 + random(game_size[1] - 200);
+    pipe_gab = 200 + random(50);
+  }
+  image(pipe_top, pipe[0], pipe[1]- pipe_top.height);
+  image(pipe_bottom, pipe[0], pipe[1]+ pipe_gab);
+
+  if(mouseIsPressed){
+    jump = 10;
+  }
+  if(jump > -20){
+    jump -= 1;
+  }
+  playercoordinates[1] -= jump;
+  image(player, playercoordinates[0], playercoordinates[1]);
 }
 
 function draw() {
   image(background, 0, 0);
-  image(player, 100, 450);
+  
   if (!game_running) {
     textAlign(CENTER, CENTER);
     textSize(40);
     fill(255);
-    text("Flying Guinea Pigs", game_size[0] / 3.5, game_size[1] / 5);
-    text("Click to start", game_size[0] / 3.5, game_size[1] / 3);
+    text("Flying Guinea Pigs", 225, 200);
+    text("Click to start", 225, 300);
     if (mouseIsPressed) {
       game_running = true;
     }
