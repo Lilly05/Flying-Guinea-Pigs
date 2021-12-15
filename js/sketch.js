77b7a26bf020e0bc0120e77276c423b7def0eb94
Lilly05 <lilly.koller@icloud.com>
2021-12-15 14:30:25 +0100

    added game over

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,8 @@
 let background, player, pipe_bottom, pipe_top;
 let game_size = [572, 1014];
 let game_running = false;
+let game_started = false;
+let GameOver = false;
 let pipe = [800, 280];
 let pipe_gab = 200;
 let pipe_speed = 5;
@@ -15,10 +17,11 @@ function preload() {
   pipe_bottom = loadImage("media/bottom_pipe.png");
   pipe_top = loadImage("media/top_pipe.png");
   font = loadFont('media/Crumbled-Pixels.ttf');
+  GameOverFont = loadFont('media/PIXEL-LI.TTF');
 }
 
 function setup() {
-  game_size = [450, windowHeight];
+  game_size = [500, windowHeight];
   createCanvas(game_size[0], game_size[1]);
   background.resize(0, game_size[1]);
   player.resize(0, 125);
@@ -34,9 +37,7 @@ function play() {
   image(pipe_top, pipe[0], pipe[1]- pipe_top.height); // 
   image(pipe_bottom, pipe[0], pipe[1]+ pipe_gab); // y Koordinate von der unteren Säule
 
-  if(mouseIsPressed){
-    jump = 10;
-  }
+
   if(jump > -20){
     jump -= 1;
   }
@@ -48,27 +49,62 @@ function play() {
     pipe[0] = game_size[0];
     pipe[1] = 50 + random(game_size[1] - 200);
     pipe_gab = 200 + random(50);
+    textAlign(CENTER, CENTER);
+    textSize(100);
+    textFont(GameOverFont);
+    fill(255);
+    text("Game Over", background.width/2-30, 200);
+    textSize(80);
+    textFont(font);
+    fill(255);
+    text("Press ENTER to play again", background.width/2 - 30, 600);
     game_running = false;
+    GameOver = true;
   }
+  
 }
 
 function draw() {
   image(background, 0, 0);
   
   if (!game_running) {
+    if (GameOver == true){
+      textAlign(CENTER, CENTER);
+      textSize(100);
+      textFont(GameOverFont);
+      fill(255);
+      text("Game Over", background.width/2-30, 200);
+      textSize(50);
+      textFont(font);
+      fill(255);
+      text("Press ENTER to play again", background.width/2 - 30, 600);
+    }else{
     textAlign(CENTER, CENTER);
     textSize(60);
     textFont(font);
     fill(255);
-    text("Flying Guinea Pigs", 225, 200);
-    text("Click to start", 225, 300);
-    if (mouseIsPressed) {
-      game_running = true;
+    text("Flying Guinea Pigs", background.width/2 - 30, 200);
+    text("Press SPACE to start", background.width/2 - 30, 600);
     }
+    
   } else {
     play();
   }
 }
+
+function keyPressed(){
+  if(GameOver == false){
+  if(key == ' '){
+    game_running = true;
+      jump = 13;
+  }
+}
+  if(GameOver == true){
+    if(keyCode === ENTER){
+      GameOver = false;
+    }
+    }
+  }
 
 function collision(im1,x1,y1, im2,x2,y2){
   if((x1+im1.width < x2) || (x1 > x2+im2.width)||(y1+im1.height < y2) || (y1 > y2 +im2.height)){
