24d359d0330b8605c2cfb25a8a7c88fc4d3251ba
Lilly05 <lilly.koller@icloud.com>
2021-12-16 11:47:35 +0100

    added collisions

--- js/sketch.js
+++ js/sketch.js
@@ -40,15 +40,13 @@ function play() {
   image(pipe_top, pipe[0], pipe[1]- pipe_top.height); // x Koordinate von der oberen Säule
   image(pipe_bottom, pipe[0], pipe[1]+ pipe_gab); // y Koordinate von der unteren Säule
 
-
   if(jump > -20){
     jump -= 1;
   }
   playercoordinates[1] -= jump;
   image(player, playercoordinates[0], playercoordinates[1]);
 
-  if(collision(player, playercoordinates[0], playercoordinates[1], pipe_top, pipe[0], pipe[1]-pipe_top.height)||
-  collision(player, playercoordinates[0], playercoordinates[1], pipe_bottom, pipe[0], pipe[1]+ pipe_gab)){
+  if(collision(player, playercoordinates[0], playercoordinates[1], pipe_top, pipe[0], pipe[1]-pipe_top.height, pipe[1]+ pipe_gab)){
   //  pipe[0] = game_size[0];
   //  pipe[1] = 50 + random(game_size[1] - 200);
   //  pipe_gab = 200;
@@ -58,10 +56,22 @@ function play() {
   
 }
 
-function collision(im1,x1,y1, im2,x2,y2){
-  if((x1 + im1.width < x2) || (x1 > x2 + im2.width) || (y1 + im1.height < y2) || (y1 > y2 + im2.height)) {
-    return false; // Keine Kollision
-  } else {
+function collision(im1,x1,y1, im2,x2,y2, y3){
+  if((x1 + im1.width / 2) > x2) { // Hier wird geprüft: trifft die Nase des Schweins in der Horizontalen auf eine Säule
+    if(y1 > y2 && y1 < (y2 + im2.height)){ // Hier wird geprüft: fliegt das Schwein in der Vertikalen auf einer Höhe, die sich zwischen Beginn und Ende der oberen Säule befindet
+      noLoop();
+      return true;
+    }else{
+      if(y1 > y3){ // Hier noch zu prüfen: fliegt das Schwein in der Vertikalen auf einer Höhe, die sich zwischen Beginn und Ende der unteren Säule befindet
+        return true;
+        
+      }else{
+        return false;
+      }
+    }
+
+
+    /*
     console.log((x1 + im1.width < x2));
     console.log((x1 > x2 + im2.width));
     console.log((y1 + im1.height < y2));
@@ -74,10 +84,9 @@ function collision(im1,x1,y1, im2,x2,y2){
     console.log("im2.height: " + im2.height);
     console.log("y1: " + y1);
     console.log("y2: " + y2);
-    
-    noLoop(); // todo: delete
-
-    return true; // Es findet eine Kollision statt
+*/
+  } else {
+    return false; // Es findet keine Kollision statt
   }
 }
 
